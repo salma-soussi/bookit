4a0442200c5947127cc6fcd9abcbde3e634a3817
salma-soussi <soussiselma@hotmail.com>
2024-12-01 13:03:31 -0600

    Cancel a booking

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,11 +1,12 @@
 import Heading from "@/components/Heading";
-import getMyBookings from "../actions/getmybookings";
+import getMyBookings from "../actions/getMyBookings";
 import BookedRoomCard from "@/components/BookedRoomCard";
 
 const BookingPage = async () => {
   const bookings = await getMyBookings();
   return (
     <>
+      <Heading title='My bookings' />
       {bookings.length === 0 ? (
         <p className='text-gray-600 m-t-4'>You have no bookings</p>
       ) : (
--- components/BookedRoomCard.jsx
+++ components/BookedRoomCard.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import CancelBookingButton from "./CancelBookingButton";
 
 const BookedRoomCard = ({ booking }) => {
   const { room_id: room } = booking;
@@ -44,12 +45,7 @@ const BookedRoomCard = ({ booking }) => {
         >
           View Room
         </Link>
-        <button
-          href='#'
-          className='bg-red-500 text-white px-4 py-2 rounded w-full sm:w-auto text-center hover:bg-red-700'
-        >
-          Cancel Booking
-        </button>
+        <CancelBookingButton bookingId={booking.$id}/>
       </div>
     </div>
   );
